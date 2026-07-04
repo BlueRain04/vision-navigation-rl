471328bd490ca0ff43bb8846dad014cbcc184e0f
Gaida <163148621+BlueRain04@users.noreply.github.com>
2026-07-04 16:10:25 +0300

    Update train.py

--- drone/scripts/train.py
+++ drone/scripts/train.py
@@ -1,10 +1,28 @@
+import argparse
+from isaaclab.app import AppLauncher
+
+parser = argparse.ArgumentParser()
+AppLauncher.add_app_launcher_args(parser)
+args_cli, _ = parser.parse_known_args()
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
 import gymnasium as gym
+import drone
+from drone.drone_env_cfg import DroneNavEnvCfg
 from drone.learning.skrl.agent import get_agent
 from skrl.trainers.torch import SequentialTrainer
 
-env = gym.make("Drone-Nav-Direct-v0", num_envs=16)
-agent = get_agent(env, device="cuda")
+env_cfg = DroneNavEnvCfg()
+env_cfg.scene.num_envs = 16
+env_cfg.sim.device = "cuda:0"
+
+env = gym.make("Drone-Nav-Direct-v0", cfg=env_cfg)
+agent = get_agent(env, device="cuda:0")
 agent.init()
+
 trainer_cfg = {"timesteps": 10000, "headless": True}
 trainer = SequentialTrainer(cfg=trainer_cfg, env=env, agents=agent)
 trainer.train()
+
+simulation_app.close()
