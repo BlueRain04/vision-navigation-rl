c419307b64065146211b306674ca617a27715405
Gaida <163148621+BlueRain04@users.noreply.github.com>
2026-07-07 17:18:06 +0300

    Update __init__.py

--- drone/__init__.py
+++ drone/__init__.py
@@ -1,12 +1,12 @@
 import gymnasium as gym
-from .drone_env import DroneNavEnv
-from .drone_env_cfg import DroneNavEnvCfg
+from .drone_env import QuadcopterEnv
+from .drone_env_cfg import QuadcopterEnvCfg
 
 gym.register(
     id="Drone-Nav-Direct-v0",
-    entry_point=f"{__name__}.drone_env:DroneNavEnv",
+    entry_point=f"{__name__}.drone_env:QuadcopterEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.drone_env_cfg:DroneNavEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.drone_env_cfg:QuadcopterEnvCfg",
     },
 )
