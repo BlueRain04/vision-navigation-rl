28027fadaf900064c4f67fbd6ce55980bea61974
Gaida <163148621+BlueRain04@users.noreply.github.com>
2026-07-10 08:21:17 +0300

    Update drone_env.py

--- drone/drone_env.py
+++ drone/drone_env.py
@@ -163,7 +163,7 @@ class QuadcopterEnv(DirectRLEnv):
         self._thrust[:, 0, 2] = self.cfg.thrust_to_weight * self._robot_weight * (self._actions[:, 0] + 1.0) / 2.0 #get the thrust action range [-1, 1] and map it to [0, 1] to apply in simulator, assign it as Z force since X Y are not applicable
         self._moment[:, 0, :] = self._moment_scale * self._actions[:, 1:] #take the roll, pitch, and yas from action scale them then add to moment tensor
         self.goal_marker.visualize(self.target_pos)
-        self._visualize_arrows()
+      #  self._visualize_arrows()
 
     def _visualize_arrows(self):
         goal_vec = self._get_goal_vec()
@@ -185,9 +185,9 @@ class QuadcopterEnv(DirectRLEnv):
             dim=0,
         )
 
-        self.arrows.visualize(
-            translations=locs, orientations=rots, marker_indices=indices
-        )
+       # self.arrows.visualize(
+          #  translations=locs, orientations=rots, marker_indices=indices
+       # )
         
     def _apply_action(self): #apply the action into the env
         self._robot.set_external_force_and_torque(
